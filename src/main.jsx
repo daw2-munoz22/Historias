fb5312ec7927ed296f72431e6569d3709a72d967
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2024-05-18 19:53:59 +0200

    Falta modificar el texto de la historia 3

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import ReactDOM from "react-dom/client";
 import {NextUIProvider} from "@nextui-org/react";
 import App from "./App";
 import "./index.css";
+import { GlobalProvider } from "./contexts/globalContext";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
-      <main className="purple-dark text-foreground bg-background">
-        <App />
-      </main>
+    <GlobalProvider>
+      <NextUIProvider>
+        <main className="purple-dark text-foreground">
+          <App />
+        </main>
+      </NextUIProvider>
+    </GlobalProvider>
     
   </React.StrictMode>,
 );

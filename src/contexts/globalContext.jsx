2f9a11af33613ce2cec4273bc5cfbfc0b160f9f0
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2024-05-20 10:28:57 +0200

    preparar contexto historia4

--- src/contexts/globalContext.jsx
+++ src/contexts/globalContext.jsx
@@ -8,7 +8,7 @@ export const GlobalProvider = ({ children }) => {
     const [historias, setHistorias] = useState(historiasData.historias)
     const [dataHistoria, setDataHistoria] = useState()
     return (
-        <GlobalContext.Provider value={{ historias, setHistorias, dataHistoria, setDataHistoria }}>
+        <GlobalContext.Provider value={{ historias, setHistorias }}>
             {children}
         </GlobalContext.Provider>
     )    

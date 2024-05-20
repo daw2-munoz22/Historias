8a265f8d3c0822f4c483faa8a4e4fec5e908ef26
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2024-05-20 11:23:36 +0200

    usar el controlador

--- src/components/HistoryCard.jsx
+++ src/components/HistoryCard.jsx
@@ -1,41 +1,41 @@
+import React from 'react';
 import { Button, Card, CardFooter, CardHeader, Image } from "@nextui-org/react";
 import { Pencil, Trash2 } from "lucide-react";
-import { useGlobalContext } from "../contexts/globalContext";
+import { useGlobalContext } from '../contexts/globalContext';
 
 
-export function HistoryCard({id, titulo, fecha, imagen, comentario}){
-
+export function HistoryCard({ id, titulo, fecha, imagen, comentario }) {
     const { setDataHistoria } = useGlobalContext();
-    
+
     const controladorEditarHistoria = () => {
         setDataHistoria({ id, titulo, fecha, imagen, comentario });
-      };
-          
-    return(
-      <Card id={id} isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-          <p className="text-tiny text-white/60 uppercase font-bold">Viaje a {titulo}</p>
-          <h4 className="text-white font-medium text-2xl">{fecha}</h4>
-      </CardHeader>
-      <Image
-          removeWrapper
-          alt="Card example background"
-          className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-          src={imagen}
-      />
-      <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-          <div className="w-2/3">
-              <p className="text-black text-tiny">{comentario}</p>
-          </div>
-          <div className="space-x-2">
-              <Button color="warning" variant="ghost" radius="lg" size="sm" onPress={controladorEditarHistoria}>
-                  <Pencil />
-              </Button>
-              <Button color="danger" variant="ghost" radius="lg" size="sm">
-                  <Trash2 />
-              </Button>
-          </div>
-      </CardFooter>
-  </Card>
-    )
+    };
+
+    return (
+        <Card id={id} isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
+            <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <p className="text-tiny text-white/60 uppercase font-bold">Viaje a {titulo}</p>
+                <h4 className="text-white font-medium text-2xl">{fecha}</h4>
+            </CardHeader>
+            <Image
+                removeWrapper
+                alt="Card example background"
+                className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+                src={imagen}
+            />
+            <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                <div className="w-2/3">
+                    <p className="text-black text-tiny">{comentario}</p>
+                </div>
+                <div className="space-x-2">
+                    <Button color="warning" variant="ghost" radius="lg" size="sm" onPress={controladorEditarHistoria}>
+                        <Pencil />
+                    </Button>
+                    <Button color="danger" variant="ghost" radius="lg" size="sm">
+                        <Trash2 />
+                    </Button>
+                </div>
+            </CardFooter>
+        </Card>
+    );
 }

013f99ce0133a30ec6b0fe41e11a97e7608fbbad
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2024-05-27 17:01:41 +0200

    TASCA 1. Llegir històries Finalizada

--- src/contexts/globalContext.jsx
+++ src/contexts/globalContext.jsx
@@ -1,17 +1,32 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useEffect, useContext, createContext } from "react";
 
-import historiasData from '../../db.json'
-
+// Creación del contexto
 const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
-    const [historias, setHistorias] = useState(historiasData.historias)
-    const [dataHistoria, setDataHistoria] = useState()
+    const [historias, setHistorias] = useState([]);
+    const [dataHistoria, setDataHistoria] = useState(); 
+
+    // useEffect para obtener los datos de la API al inicializar el componente
+    useEffect(() => {
+        const fetchHistorias = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/historias');
+                const data = await response.json();
+                setHistorias(data);
+            } catch (error) {
+                console.error('Error fetching historias:', error);
+            }
+        };
+        
+        fetchHistorias();
+    }, []); // El array vacío [] asegura que este efecto solo se ejecute una vez al montar el componente
+
     return (
         <GlobalContext.Provider value={{ historias, setHistorias, dataHistoria, setDataHistoria }}>
             {children}
         </GlobalContext.Provider>
-    )    
+    );    
 }
 
 export const useGlobalContext = () => {
